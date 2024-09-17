c92560a57d9ddd582a82a0d40f3312c1ea11cbe6
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-17 23:21:20 +0800

    create a sign in view

---
+++ src/views/sign-in/sign-in.styles.ts
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import Button from "../components/button/button";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+export const GoogleButton = styled(Button)`
+  display: flex;
+  align-items: center;
+  background-color: #4285f4;
+
+  &:hover {
+    background-color: #357ae8;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  img {
+    margin-right: 10px;
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+export const Icon = styled(FontAwesomeIcon)`
+  margin-right: 8px;
+`;
---
+++ src/views/sign-in/sign-in.tsx
@@ -0,0 +1,16 @@
+import { signInUser } from "@/utils/auth";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import * as S from "./sign-in.styles";
+
+const SignInWithGoogle = () => {
+  return (
+    <S.Container>
+      <S.GoogleButton onClick={signInUser}>
+        <S.Icon icon={faGoogle} />
+        Sign in with Google
+      </S.GoogleButton>
+    </S.Container>
+  );
+};
+
+export default SignInWithGoogle;

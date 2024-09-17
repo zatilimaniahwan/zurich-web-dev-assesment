9edc542629ad8115bac3548f49e03e5447c1e357
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-18 00:45:38 +0800

    add js for sign out page

--- src/views/sign-out/sign-out.tsx
+++ src/views/sign-out/sign-out.tsx
@@ -4,6 +4,12 @@ import { useDispatch } from "react-redux";
 import * as S from "./sign-out.styles";
 import { useRouter } from "next/router";
 
+/**
+ * Renders a Sign Out button that redirects to the sign-in page and logs out the user
+ * using the `signOutUser` function.
+ *
+ * @returns A button element with a "Sign Out" label.
+ */
 const SignOut = () => {
   const dispatch = useDispatch<AppDispatch>();
   const router = useRouter();

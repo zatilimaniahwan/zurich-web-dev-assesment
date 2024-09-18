220a4cb3907ccadb13d04c020d83123af3f36342
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-18 21:55:33 +0800

    add jsdocs for footer n header

--- src/views/footer/footer.tsx
+++ src/views/footer/footer.tsx
@@ -1,5 +1,10 @@
 import FooterComponent from "../components/footer-component/footer-component";
 
+/**
+ * The Footer component renders a simple footer with the current year.
+ *
+ * @returns {JSX.Element} The JSX element representing the Footer component.
+ */
 const Footer = () => (
   <FooterComponent>
     <p>© {new Date().getFullYear()} Zurich Malaysia</p>
--- src/views/header/header.tsx
+++ src/views/header/header.tsx
@@ -5,6 +5,11 @@ import Link from "next/link";
 import * as S from "./header.styles";
 import SignOut from "../sign-out/sign-out";
 
+/**
+ * A functional component that renders the header of the application.
+ * It displays the Zurich Insurance logo and a navigation bar with a link to sign out.
+ * If a user is signed in, it displays a welcome message with the user's name.
+ */
 const Header = () => {
   const user = useSelector((state: RootState) => state.userData.user?.name);
   return (

f6a008aba1327ecbb44d37c27528f549a8b64961
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-24 02:42:22 +0800

    update unauthorized message to use tailwind

--- src/views/unauthorized-message/unauthorized-message.tsx
+++ src/views/unauthorized-message/unauthorized-message.tsx
@@ -1,7 +1,7 @@
-import * as S from "./unauthorized-message.styles";
 type UnauthorizedMessageProps = {
   handleRedirect: () => void;
 };
+
 /**
  * The UnauthorizedMessage component is used to display an unauthorized access
  * message to users who do not have permission to access the system.
@@ -10,13 +10,16 @@ type UnauthorizedMessageProps = {
  * @returns {JSX.Element} The JSX element representing the UnauthorizedMessage component.
  */
 const UnauthorizedMessage = ({ handleRedirect }: UnauthorizedMessageProps) => (
-  <S.UnauthorizedContainer>
-    <S.UnauthorizedTitle>Unauthourized Access</S.UnauthorizedTitle>
-    <S.UnauthorizedMessage>
-      You do not have permission to access this system.
-    </S.UnauthorizedMessage>
-    <S.BackButton onClick={handleRedirect}>Back</S.BackButton>
-  </S.UnauthorizedContainer>
+  <div className="flex flex-col justify-center items-center h-screen text-red-600 text-center">
+    <h1 className="text-5xl mb-5">Unauthorized Access</h1>
+    <p className="mb-5">You do not have permission to access this system.</p>
+    <button
+      onClick={handleRedirect}
+      className="bg-red-600 text-white py-2 px-4 rounded hover:bg-red-500 focus:outline-none"
+    >
+      Back
+    </button>
+  </div>
 );
 
 export default UnauthorizedMessage;

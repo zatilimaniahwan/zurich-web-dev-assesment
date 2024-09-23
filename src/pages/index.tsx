339e63a0365e70338102ca2dbe002a3147ca0b5f
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-24 01:56:59 +0800

    redirect index -> users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,14 @@
-import { clearUser, setUser } from "@/store/slices/user-slice";
-import { AppDispatch } from "@/store";
-import Users from "@/views/users/users";
-import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import Footer from "@/views/footer/footer";
-import UseFetchUsersData from "./api/hooks/use-fetch-users-data";
-import Header from "@/views/header/header";
-import { maskEmailAddress, toggleVisibleEmail } from "@/utils/email-utils";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Index = () => {
-  const { status, data: session } = useSession();
-  const dispatch = useDispatch<AppDispatch>();
-  const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [visibleEmails, setVisibleEmails] = useState<Record<number, boolean>>(
-    {}
-  );
-
-  /**
-   * Fetches all the users from the API.
-   * Sets the users state and the loading state.
-   * If there is an error, logs the error to the console.
-   * Finally, sets the loading state to false.
-   */
-  const fetchUsers = async () => {
-    setLoading(true);
-    try {
-      const users = await UseFetchUsersData();
-      setUsers(users);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error loading users:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const router = useRouter();
 
   useEffect(() => {
-    if (status === "authenticated" && session?.user) {
-      // Set the user in Redux store
-      dispatch(setUser(session.user as GoogleUserProfile));
-      if (users.length === 0) fetchUsers();
-      // Redirect to users page if authenticated
-    } else if (status === "unauthenticated") {
-      // Clear user in Redux store if not authenticated
-      dispatch(clearUser());
-    }
-  }, [session, status, dispatch, users]);
+    router.push("/users");
+  }, [router]);
 
-  if (status === "authenticated" && session?.user) {
-    return (
-      <>
-        <Header />
-        <Users
-          users={users}
-          loading={loading}
-          toggleVisibleEmail={(userId: number) =>
-            toggleVisibleEmail(userId, setVisibleEmails)
-          }
-          maskEmailAddress={maskEmailAddress}
-          visibleEmails={visibleEmails}
-          setVisibleEmails={setVisibleEmails}
-        />
-        <Footer />
-      </>
-    );
-  }
+  return null;
 };
 
 export default Index;

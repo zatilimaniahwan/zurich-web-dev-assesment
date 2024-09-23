6660cdb3d7fc2794e87e308224019c4af793e98f
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-24 05:18:05 +0800

    create a user card component

---
+++ src/views/components/user-card/user-card.tsx
@@ -0,0 +1,44 @@
+import { memo } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+// Use memo to prevent unnecessary re-renders
+const UserCard = memo(
+  ({
+    user,
+    isEmailVisible,
+    setShowEmailId,
+  }: {
+    user: User;
+    isEmailVisible: boolean;
+    setShowEmailId: React.Dispatch<React.SetStateAction<number | null>>;
+  }) => {
+    return (
+      <div
+        className="bg-white border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center text-center
+        w-full sm:w-[calc(50%-20px)] md:w-[calc(33.333%-20px)]"
+      >
+        <img
+          src={user.avatar}
+          alt={`${user.first_name} ${user.last_name}`}
+          className="rounded-full w-20 h-20"
+        />
+        <h3 className="mt-2 text-lg">
+          {user.first_name} {user.last_name}
+        </h3>
+        <p className="mt-1 text-gray-600 w-full flex items-center justify-center">
+          {user.email}
+          <FontAwesomeIcon
+            icon={isEmailVisible ? faEyeSlash : faEye}
+            className="ml-2 mt-2 cursor-pointer"
+            onClick={() => setShowEmailId(isEmailVisible ? null : user.id)}
+          />
+        </p>
+      </div>
+    );
+  }
+);
+
+UserCard.displayName = "UserCard";
+
+export default UserCard;

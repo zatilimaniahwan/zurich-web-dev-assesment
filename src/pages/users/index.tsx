1fb342b11cf012e5808181dc0088e253d415fbb7
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-24 05:16:35 +0800

    update the users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,49 +5,46 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import Footer from "@/views/footer/footer";
-import UseFetchUsersData from "../api/hooks/use-fetch-users-data";
 import Header from "@/views/header/header";
-import { maskEmailAddress, toggleVisibleEmail } from "@/utils/email-utils";
+import axios from "axios";
 
 const Index = () => {
   const { status, data: session } = useSession();
   const dispatch = useDispatch<AppDispatch>();
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [visibleEmails, setVisibleEmails] = useState<Record<number, boolean>>(
-    {}
-  );
-
-  /**
-   * Fetches all the users from the API.
-   * Sets the users state and the loading state.
-   * If there is an error, logs the error to the console.
-   * Finally, sets the loading state to false.
-   */
-  const fetchUsers = async () => {
-    setLoading(true);
-    try {
-      const users = await UseFetchUsersData();
-      setUsers(users);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error loading users:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [showEmailId, setShowEmailId] = useState<number | null>(null);
 
   useEffect(() => {
+    /**
+     * Fetches the list of users from the API with the showEmailId query parameter.
+     * The showEmailId is used to conditionally show the email address for the user
+     * with the matching ID.
+     */
+    const fetchUsers = async () => {
+      setLoading(true);
+      try {
+        // Pass the showEmailId as a query parameter
+        const { data } = await axios.get<User[]>(
+          `/api/users?showEmailId=${showEmailId}&showEmail=${
+            showEmailId !== null
+          }`
+        );
+        setUsers(data);
+      } catch (error) {
+        console.error("Error loading users:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     if (status === "authenticated" && session?.user) {
-      // Set the user in Redux store
       dispatch(setUser(session.user as GoogleUserProfile));
-      if (users.length === 0) fetchUsers();
-      // Redirect to users page if authenticated
+      fetchUsers();
     } else if (status === "unauthenticated") {
-      // Clear user in Redux store if not authenticated
       dispatch(clearUser());
     }
-  }, [session, status, dispatch, users]);
+  }, [session, status, dispatch, showEmailId]);
 
   if (status === "authenticated" && session?.user) {
     return (
@@ -56,17 +53,15 @@ const Index = () => {
         <Users
           users={users}
           loading={loading}
-          toggleVisibleEmail={(userId: number) =>
-            toggleVisibleEmail(userId, setVisibleEmails)
-          }
-          maskEmailAddress={maskEmailAddress}
-          visibleEmails={visibleEmails}
-          setVisibleEmails={setVisibleEmails}
+          showEmailId={showEmailId}
+          setShowEmailId={setShowEmailId}
         />
         <Footer />
       </>
     );
   }
+
+  return null;
 };
 
 export default Index;
